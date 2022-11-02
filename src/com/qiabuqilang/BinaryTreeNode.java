7f5bf7da096a270e31f6891745bea13db4e367eb
qiabuqilang <qiabuqilang@gmail.com>
2022-11-02 16:32:23 +0800

    add tree 00

---
+++ src/com/qiabuqilang/BinaryTreeNode.java
@@ -0,0 +1,210 @@
+package com.qiabuqilang;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BinaryTreeNode {
+    private int data;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public BinaryTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryTreeNode left) {
+        this.left = left;
+    }
+
+    public BinaryTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryTreeNode right) {
+        this.right = right;
+    }\
+
+    //前序遍历
+    public void preOrder(BinaryTreeNode root){
+        if(root != null){
+            System.out.println(root.getData());
+            preOrder(root.getLeft());
+            preOrder(root.getRight());
+        }
+    }
+    //非递归前序遍历
+    public void preOrderNonRecursive(BinaryTreeNode root){
+        if(root == null){
+            return;
+        }
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        while(true){
+            while(root != null){
+                System.out.println(root.getData());
+                stack.push(root);
+                root = root.getLeft();
+            }
+            if(stack.isEmpty()){
+                break;
+            }
+            root = (BinaryTreeNode) stack.pop();
+            root = root.getRight();
+        }
+        return;
+    }
+    //中序遍历
+    public void inOrder(BinaryTreeNode root){
+        if(root != null){
+            inOrder(root.getLeft());
+            System.out.println(root.getData());
+            inOrder(root.getRight());
+        }
+    }
+    //非递归中序遍历
+    public void inOrderNonRecursive(BinaryTreeNode root){
+        if(root == null) return;
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        while(true){
+            while(root != null){
+                stack.push(root);
+                root = root.getLeft();
+            }
+            if(stack.isEmpty()){
+                break;
+            }
+            root = (BinaryTreeNode) stack.pop();
+            System.out.println(root.getData());
+            root = root.getRight();
+        }
+        return;
+    }
+    //后序遍历
+    public void postOrder(BinaryTreeNode root){
+        if(root != null){
+            postOrder(root.getLeft());
+            postOrder(root.getRight());
+            System.out.println(root.getData());
+        }
+    }
+    //非递归后续遍历
+    public void postOrderNonRecursive(BinaryTreeNode root){
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        while (true){
+            if(root != null){
+                stack.push(root);
+                root = root.getLeft();
+            }else{
+                if(stack.isEmpty()){
+                    System.out.println("stack is empty");
+                    return;
+                }else{
+                    if(stack.peek().getRight() == null){
+                        root = stack.pop();
+                        System.out.println(root.getData());
+
+                    }
+                    if(!stack.isEmpty()){
+                        root = stack.peek().getRight();
+                        root = null;
+                    }
+                }
+
+            };
+        }
+    }
+
+    //层次遍历
+    public void levelOrder(BinaryTreeNode root){
+        BinaryTreeNode temp;
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            temp = queue.remove();
+            System.out.println(temp.getData());
+            if(temp.getLeft() !=null){
+                queue.add(temp.getLeft());
+            }
+            if(temp.getRight() != null){
+                queue.add(temp.getRight());
+            }
+        }
+
+    }
+
+    //查找二叉树最大
+    public int findMax(BinaryTreeNode root){
+        int root_val,left,right,max = Integer.MIN_VALUE;
+        if(root != null){
+            root_val = root.getData();
+            left = findMax(root.getLeft());
+            right = findMax(root.getRight());
+            if(left>right){
+                max = left;
+            }else{
+                max = right;
+            }
+            if(root_val>max){
+                max = root_val;
+            }
+        }
+        return max;
+    }
+
+    //层次遍历获取最大
+    public int findMaxUsingLevelOrder(BinaryTreeNode root){
+        BinaryTreeNode temp;
+        int max = Integer.MIN_VALUE;
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            temp = queue.remove();
+            if(max < temp.getData()){
+                max = temp.getData();
+            }
+            if(temp.getLeft() != null){
+                queue.add(temp.getLeft());
+            }
+            if(temp.getRight() != null){
+                queue.add(temp.getRight());
+            }
+        }
+        return max;
+    }
+
+    //搜索元素
+    public Boolean findInBinaryTreeUsingRecursion(BinaryTreeNode root,int data){
+        Boolean temp;
+        if(root == null){
+            return false;
+        }else{
+            if(data == root.getData()){
+                return true;
+            }else{
+                temp = findInBinaryTreeUsingRecursion(root.getLeft(),data);
+                if(temp != true){
+                    return temp;
+                }else{
+                    return findInBinaryTreeUsingRecursion(root.getRight(),data);
+                }
+            }
+        }
+
+    }
+
+    //层次遍历查找最大
+   
+}
